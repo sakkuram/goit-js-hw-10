3dc00dad811acbd2473315d8907c997218b182fd
[den] <114509752+sakkuram@users.noreply.github.com>
2023-12-18 18:55:31 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const breedInfoDiv = document.getElementById('breed-info');
 const catImage = document.createElement('img');
 const catDescription = document.createElement('p');
 const loaderElement = document.querySelector('p.loader');
-const errorElement = document.querySelector('p.error');//12345
+const errorElement = document.querySelector('p.error');//123456
 
 // Hide loader and error initially
 loaderElement.style.display = 'none';
