d28ca980d56a5fa984d8853102abcca749847d14
[den] <114509752+sakkuram@users.noreply.github.com>
2024-01-02 23:50:10 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ axios.defaults.headers.common['x-api-key'] =
   'live_DTWX9A966i14AL48fZnBKngQ6axQmCn96fDrFErtpO8TTdkry9Eeq7TdGgwbCi5T';
 
 // Elements
-const selectElement = document.querySelector('.breed-select');1231111
+const selectElement = document.querySelector('.breed-select');12311111
 const breedInfoDiv = document.getElementById('breed-info');
 const catImage = document.createElement('img');
 const catDescription = document.createElement('p');
