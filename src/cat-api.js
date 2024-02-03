7d948eeb57c0d39a2db185756b28428fe9b1791b
[den] <114509752+sakkuram@users.noreply.github.com>
2024-02-03 23:18:12 +0200

    Update cat-api.js

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
          
-axios.defaults.headers.common['x-api-key'] = 'your API key'; 
+axios.defaults.headers.common['x-api-key'] = 'your API key';  
  
 // Function to fetch cat breeds
 export function fetchBreeds() {
